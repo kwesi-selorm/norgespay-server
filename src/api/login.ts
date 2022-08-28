3b3b3230e9a032fcb7edc6aa6597b0c696b5935d
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-28 18:55:11 +0200

    Change login and authentication techniques to sessions on the server side

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,39 +1,66 @@
-import jwt from "jsonwebtoken";
 import express from "express";
-import User from "../models/user-model";
 import { validatePassword } from "../fns/password-fns";
-import { SECRET } from "../utils/config";
-import { LoginRequest } from "../types/types";
+import Contributor from "../models/contributor-model";
+import User from "../models/user-model";
 import { loginParser } from "../parsers";
-import { AppError } from "../utils/classes/AppError";
+import { SessionUser } from "../types/types";
+import { AppError } from "../utils/error-classes";
+
+declare module "express-session" {
+    interface SessionData {
+        user: SessionUser;
+    }
+}
 
 const loginRouter = express.Router();
 
-loginRouter.post("/", async (req: LoginRequest, res, next) => {
-  const result = loginParser(req, next);
-  if (result) {
-    const { username, password } = result;
+/* 
+This is a post request to the login route. It is using the loginParser function to parse the request body and return the username and password. It then uses the username to find the user in the
+database. If the user is found, it uses the validatePassword function to check if the password is
+correct. If the password is correct, it creates a payload with the username and id. It then creates
+an access token and a refresh token. It then creates a secure cookie with the refresh token. It then
+sends a response with the access token, username, and id. 
+*/
 
-    try {
-      const user = await User.findOne({ username });
-      if (user == null) {
-        return next(new AppError("User not found", 404));
-      }
-      const match = await validatePassword(password, user.passwordHash);
-      if (!match) {
-        return res.status(401).send({ message: "Invalid username or password" });
-      }
-      const tokenUser = {
-        username,
-        id: user._id,
-      };
-      const token = jwt.sign(tokenUser, SECRET);
-      return res.status(200).send({ token, username: user.username, id: user._id });
-    } catch (error) {
-      if (error instanceof Error)
-        next(new AppError("Login failed, possibly invalid username or password", 500));
-    }
-  }
-});
+loginRouter.post(
+    "/",
+    async (req: express.Request, res: express.Response, next) => {
+        const result = loginParser(req, next);
+        if (!result) return;
+
+        const { username, password } = result;
+        try {
+            const user = await User.findOne({ username });
+            if (user == null) return next(new AppError("User not found", 404));
+            const match = await validatePassword(password, user.passwordHash);
+            if (!match)
+                return next(new AppError("Invalid username or password", 401));
+
+            let contributor = false;
+            const document = await Contributor.findOne({
+                username,
+            });
+            if (document) contributor = true;
+
+            const authUser = {
+                username: user.username,
+                id: user._id.toString(),
+                contributed: contributor,
+            };
+
+            req.session.user = authUser;
+            console.log(req.session);
+            res.json(authUser);
+        } catch (error) {
+            if (error instanceof Error)
+                next(
+                    new AppError(
+                        `Login failed, please try again later: ${error.message}`,
+                        500,
+                    ),
+                );
+        }
+    },
+);
 
 export default loginRouter;
