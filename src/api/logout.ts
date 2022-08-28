adcabea706ade288dc1143767640f62c0a0889c6
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-08-28 18:53:49 +0200

    Set up log out controller

---
+++ src/api/logout.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const logoutRouter = express.Router();
+
+logoutRouter.post("/", (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies["connect.sid"]) return res.sendStatus(204);
+    res.clearCookie("connect.sid", {
+        secure: false,
+        maxAge: 30 * 1000,
+        httpOnly: true,
+        sameSite: "lax",
+    }).send();
+});
+
+export default logoutRouter;
